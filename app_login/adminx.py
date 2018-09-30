0898b286c13c2bdf13b6221e48c038b96aa5b4d0
SpiderKin <end_moon@163.com>
2018-09-30 15:40:15 +0800

    xadmin后台管理
    Signed-off-by: SpiderKin <end_moon@163.com>

--- app_login/adminx.py
+++ app_login/adminx.py
@@ -1,5 +1,7 @@
 import xadmin
 from .models import User
+from .models import Confirm
 # class Useradmin():
 #     list_display=['name','email',]
-xadmin.site.register(User)
+xadmin.site.register(User)          #将用户表注册到xadmin后台
+xadmin.site.register(Confirm)        #将用户确认表注册到xadmin后台
